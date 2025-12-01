1746c2174ca1c62ebcd5c2ff9bd160477c2354a0
Amir Loong <amirloong421@gmail.com>
2025-12-01 21:41:02 +0800

    still not done

---
+++ book_api.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+based_url = "https://openlibrary.org/search.json"
+
+def book():
+    my_param = {"q": "Harry Potter", "limit": 1}
+
+    response = requests.get(based_url, params=my_param)
+
+    if response.status_code == 200:
+        data = response.json()
+        print(json.dumps(data, indent=4))
+        
+book()
